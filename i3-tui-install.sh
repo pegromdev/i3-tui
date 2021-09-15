08fc31a28adecd6b5ed39f7ba835855cf11b147f
NiKO <pegromdev@gmail.com>
2021-09-15 02:48:45 -0300

    09-15 pre-alpha

--- i3-tui-install.sh
+++ i3-tui-install.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 # Initial Setup scritp to Manually test Endeavouros I3-TUI Community Edition.
 # maintainer: nicolas villafan <pegromdev@gmail.com>
+#########################
+# Alpha build for testing
+#########################
 
+echo "This will Build and automatically reboot when done"
 
-# Easy setup for testing
+echo "Copying configuration Files..."
 
+mkdir .config
 cp -R .config/* ~/.config/
+mkdir .bin
 cp -R .bin/* ~/.bin/
+mkdir .local/share/fonts
 cp -R fonts/* ~/.local/share/fonts/
 cp .zprofile ~/.zprofile
 cp .xinitrc ~/.xinitrc
 cp .Xresources ~/.Xresources
 cp /root/xorg.conf /etc/X11/xorg.conf 
+
+echo "Configuring system setup..."
+
 echo '#include "/home/'$USER'/.config/i3/currenti3theme"' >> ~/.Xresources
 chmod -R +x ~/.bin/
-cat packages-repository.txt | xargs sudo pacman -S --noconfirm 
 fc-cache -f -v
+ln -s ~/.zprofile .profile
+ln -s ~/.zprofile .bash_profile
+chsh -s /bin/zsh $USER
+
+echo "Installing default apps and tools.."
+
+cat packages-repository.txt | xargs sudo pacman -S --noconfirm 
+
+echo " Rebooting Now..."
+reboot
+
+
 
