5dddcffbf07f74716e333e73fe683e1491ac9fae
NiKO <pegromdev@gmail.com>
2021-09-14 22:09:08 -0300

    Testing Fonts, Fix for install script

--- i3-tui-install.sh
+++ i3-tui-install.sh
@@ -6,11 +6,14 @@
 # Easy setup for testing
 
 cp -R .config/* ~/.config/
+cp -R .bin/* ~/.bin/
+cp -R fonts/* ~/.local/share/fonts/
 cp .zprofile ~/.zprofile
 cp .xinitrc ~/.xinitrc
 cp .Xresources ~/.Xresources
-mkdir ~/.bin
-cp -R .bin/* ~/.bin/
+cp /root/xorg.conf /etc/X11/xorg.conf 
 echo '#include "/home/'$USER'/.config/i3/currenti3theme"' >> ~/.Xresources
 chmod -R +x ~/.bin/
-sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
+sudo pacman -Syu --needed --noconfirm -- < packages-repository.txt
+fc-cache -f -v
+
